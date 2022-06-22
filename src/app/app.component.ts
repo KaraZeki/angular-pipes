51ea25fa231a39511f12eebc675db81659b641c1
Mehmet Zeki Kara <46008905+KaraZeki@users.noreply.github.com>
2022-06-23 00:38:30 +0300

    pipe-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  name!:string;
-  date!:string;
-  amount!:string;
-  onNameChange(value:Event){
-    var data =(value.target as HTMLInputElement).value;
-    this.name=data;
+  name!: string;
+  date!: string;
+  amount!: string;
+  height!: string;
+  onNameChange(value: Event) {
+    var data = (value.target as HTMLInputElement).value;
+    this.name = data;
   }
-  onDateChange(value:Event){
-    var data =(value.target as HTMLInputElement).value;
-    this.date=data;
+  onDateChange(value: Event) {
+    var data = (value.target as HTMLInputElement).value;
+    this.date = data;
   }
-  onAmountChange(value:Event){
-    var data =(value.target as HTMLInputElement).value;
-    this.amount=data;
+  onAmountChange(value: Event) {
+    var data = (value.target as HTMLInputElement).value;
+    this.amount = data;
+  }
+  onHeigtChange(value: Event) {
+    var data = (value.target as HTMLInputElement).value;
+    this.height = data;
   }
 }
