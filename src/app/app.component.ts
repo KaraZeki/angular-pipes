d619464e54f659bb6dcad8649f4d563667f36cd2
Mehmet Zeki Kara <46008905+KaraZeki@users.noreply.github.com>
2022-06-23 00:28:43 +0300

    angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name!:string;
+  date!:string;
+  amount!:string;
+  onNameChange(value:Event){
+    var data =(value.target as HTMLInputElement).value;
+    this.name=data;
+  }
+  onDateChange(value:Event){
+    var data =(value.target as HTMLInputElement).value;
+    this.date=data;
+  }
+  onAmountChange(value:Event){
+    var data =(value.target as HTMLInputElement).value;
+    this.amount=data;
+  }
 }
